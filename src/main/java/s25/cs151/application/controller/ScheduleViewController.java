75a14306370828f6f541ee208b9ee743e030ae33
omaradrianne <oacbapora@gmail.com>
2025-04-12 09:45:36 -0700

    View schedule page output update.

--- src/main/java/s25/cs151/application/controller/ScheduleViewController.java
+++ src/main/java/s25/cs151/application/controller/ScheduleViewController.java
@@ -1,13 +1,17 @@
 package s25.cs151.application.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import s25.cs151.application.CommonObjects;
 import s25.cs151.application.Main;
+import s25.cs151.application.Schedule;
 
 import java.io.IOException;
 
@@ -23,6 +27,19 @@ public class ScheduleViewController {
 
     @FXML
     public TableColumn timeSlot;
+
+    @FXML
+    public void initialize() {
+
+        CommonObjects commonObject = CommonObjects.getInstance();
+        ObservableList<Schedule> scheduleCSVList = commonObject.getScheduleCSVList();
+
+        // Set up columns
+        scheduleDate.setCellValueFactory(new PropertyValueFactory<>("scheduleDate"));
+        timeSlot.setCellValueFactory(new PropertyValueFactory<>("timeSlot"));
+
+        table.setItems(scheduleCSVList);
+    }
 
     @FXML
     public void onExitClick(ActionEvent actionEvent) {
