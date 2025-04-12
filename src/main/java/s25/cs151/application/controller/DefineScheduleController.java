edb12109dfbbba2196851663200a872942a9b88f
omaradrianne <oacbapora@gmail.com>
2025-04-12 01:07:52 -0700

    Update homepage, create view-schedule.fxml, transition implemented

--- src/main/java/s25/cs151/application/controller/DefineScheduleController.java
+++ src/main/java/s25/cs151/application/controller/DefineScheduleController.java
@@ -2,11 +2,16 @@ package s25.cs151.application.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import s25.cs151.application.Main;
+
+import java.io.IOException;
 
 public class DefineScheduleController {
 
@@ -23,6 +28,9 @@ public class DefineScheduleController {
     public ComboBox timeSlotBox;
 
     @FXML
+    public ComboBox courseBox;
+
+    @FXML
     public TextArea reasonTextArea;
 
     @FXML
@@ -34,5 +42,15 @@ public class DefineScheduleController {
 
     @FXML
     public void onCancelClick(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home-page.fxml"));
+            Pane pane = fxmlLoader.load();
+
+            schedulePane.getChildren().clear();
+            schedulePane.getChildren().add(pane);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
